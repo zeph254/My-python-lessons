b55fc59832af0d381a9e1c860a1a844bb4b06190
zeph254 <ularezehaniah@gmail.com>
2025-05-17 10:04:47 +0300

    welcome.py

---
+++ welcome.py
@@ -0,0 +1,20 @@
+name = "dave"
+age = 30
+print(name)
+print(age)
+
+line1 = '**************'
+line2 = '**************'
+line4 = '*            *'
+line5 = '*  WELCOME   *'
+line6 = '*            *'
+line7 = '**************'
+line8 = '**************'
+
+print(line1)
+print(line2)
+print(line4)
+print(line5)
+print(line6)
+print(line7)
+print(line8)
